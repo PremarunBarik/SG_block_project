a29b09050eade71283365a57e1a92df6dceb1887
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2023-02-28 17:04:21 -0600

    Update Initialization_3layer.jl

--- Initialization_3layer.jl
+++ Initialization_3layer.jl
@@ -1,1 +1,24 @@
+using Plots
+using Random
 
+rng = MersenneTwister(1234)
+
+#Initialization of the SG cube
+Lx = 10
+Ly = 10
+Lz = 10
+
+#initialization the number of spins
+N = 300
+mx = Int64[ (-1)^rand(rng, Int64) for i in 1:N]
+x_pos = Float64[ rand(rng, Float64)*Lx for i in 1:Lx]
+y_pos = Float64[ rand(rng, Float64)*Ly for i in 1:Ly]
+z_pos = Float64[ rand(rng, Float64)*Lz for i in 1:Lz]
+
+
+#Creating file to save the spin positions
+open("spin_positions.txt", "w") do io
+  for i in 1:Lx
+    println(io,x_pos[i]"\t",y_pos[i],"\t",z_pos[i])
+  end
+end
