7dad874797466e4e99aaa1ea6c267246fa06f8b7
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2023-03-02 00:24:27 -0600

    Create Initialization_lattice_layer.jl

---
+++ Initialization_lattice_layer.jl
@@ -0,0 +1,24 @@
+using Random
+
+rng = MersenneTwister(1234)
+
+#INITIALIZATION OF THE LATTICE
+Lx = 10
+Ly = 10
+Lz = 3
+
+#NUMBER OF SPINS
+N = Lx*Ly*Lz
+
+#SPIN-GLASS SPIN POSITIONS
+x_pos = collect(1:Lx)
+x_pos = repeat(x_pos, inner=(Lx,1))
+x_pos = repeat(x_pos, outer=(Lz,1))
+y_pos = collect(1:Ly)
+y_pos = repeat(y_pos, outer=(Lx,1))
+y_pos = repeat(y_pos, outer=(Lz,1))
+z_pos = collect(1:Lz)
+z_pos = repeat(z_pos, inner=(Lx*Ly,1))
+
+#PRINTING SPIN POSITIONS
+open(
