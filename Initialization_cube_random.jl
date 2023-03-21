2f85dce00a41fbbf981934be53719ed34637574e
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2023-03-20 21:59:18 -0500

    Update Initialization_cube_random.jl

--- Initialization_cube_random.jl
+++ Initialization_cube_random.jl
@@ -1,4 +1,4 @@
-using Plots
+using GLMakie, Random, LinearAlgebra
 using Random
 
 rng = MersenneTwister(1234)
@@ -6,19 +6,20 @@ rng = MersenneTwister(1234)
 #Initialization of the SG cube
 Lx = 10
 Ly = 10
-Lz = 10
+Lz = 3
 
 #initialization the number of spins
-N = 1000
-mx = Int64[ (-1)^rand(rng, Int64) for i in 1:N]
+N = Lx*Ly*Lz
+
+#Initialization of spin positions
 x_pos = Float64[ rand(rng, Float64)*Lx for i in 1:N]
 y_pos = Float64[ rand(rng, Float64)*Ly for i in 1:N]
 z_pos = Float64[ rand(rng, Float64)*Lz for i in 1:N]
 
+#Initialization of spin directions
+x_dir = Float64[ 2*rand(rng, Float64)-1 for i in 1:N]
+y_dir = Float64[ 2*rand(rng, Float64)-1 for i in 1:N]
+z_dir = Float64[ 2*rand(rng, Float64)-1 for i in 1:N]
 
-#Creating file to save the spin positions
-open("spin_positions.txt", "w") do io
-  for i in 1:N
-    println(io,x_pos[i],"\t",y_pos[i],"\t",z_pos[i])
-  end
-end
+#Plotting the spins
+quiver( x_pos, y_pos, z_pos, x_dir, y_dir, z_dir)
