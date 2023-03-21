73e8553372f09b013b3d12f8dfc812afd1214d89
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2023-03-21 16:28:22 -0500

    Update Initialization_cube_random.jl

--- Initialization_cube_random.jl
+++ Initialization_cube_random.jl
@@ -21,6 +21,10 @@ y_dir = Float64[ 2*rand(rng, Float64)-1 for i in 1:N]
 z_dir = Float64[ 2*rand(rng, Float64)-1 for i in 1:N]
 
 #Plotting the spins
-p = quiver!( x_pos, y_pos, z_pos, x_dir, y_dir, z_dir)
+aspect=(1, 1, 1)
+perspectiveness=0.5
+fig = Figure(; resolution=(1200, 1200))
+ax = Axis3(fig[1, 1]; aspect, perspectiveness)
+p = quiver!(ax, x_pos, y_pos, z_pos, x_dir, y_dir, z_dir)
 Makie.save("Initial_spins.png", p)
 
