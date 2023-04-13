7312e3e726bc94a3710b9bb72ea1193d0e87d311
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2023-04-13 01:03:07 -0500

    Update Impurity_inside_periodic_lattice.jl

--- Impurity_inside_periodic_lattice.jl
+++ Impurity_inside_periodic_lattice.jl
@@ -1,20 +1,22 @@
-using GLMakie, Random, AbstractAlgebra
+using Plots, Random, LinearAlgebra
 
-#VOLUME OF THE UNIT
-L_x = 100
-L_y = 100
-L_z = 30
+rng = MersenneTwister(4321)
+
+#VOLUME OF THE SPACE USED
+L_x = 10
+L_y = 10
+L_z = 3
 
 V = L_x*L_y*L_z
 
 #PERCENTAGE AND NUMBER OF UNIT CELL
-Lattice_const = 1
+Lattice_const = 1/10
 Unit_cell_along_x = convert(Int64, trunc(L_x/Lattice_const))
 Unit_cell_along_y = convert(Int64, trunc(L_y/Lattice_const))
 Unit_cell_along_z = convert(Int64, trunc(L_z/Lattice_const))
 Unit_cell_num = convert(Int64, trunc(Unit_cell_along_x * Unit_cell_along_y * Unit_cell_along_z))
 
-Imp_percent = 10                                                #WE CONSIDER THE IMPURITY PERCENTAGE TO BE 10%
+Imp_percent = 10                                               #WE CONSIDER THE IMPURITY PERCENTAGE TO BE 10%
 Imp_num = convert(Int64, trunc((Unit_cell_num*10)/100))
 
 #POSITION OF IMPURITY INSIDE AN UNIT CELL 
@@ -56,17 +58,163 @@ for i in 1:Unit_cell_num
     z_pos_base[i] = trunc((i-1) / (Unit_cell_along_y *Unit_cell_along_x))*Lattice_const
 end
 
+#IMPURITY SPIN VECTORS
+N_sg = Imp_num                                              #CHANGING THE NOTATION FROM IMPURITY TO SPIN GLASS
 
+x_dir_sg = zeros(N_sg,1)
+y_dir_sg = zeros(N_sg,1)
+z_dir_sg = zeros(N_sg,1)
 
-#scatter!(x_pos_base, y_pos_base, z_pos_base, markersize=1, legend=false)
-#scatter!(x_pos_sg, y_pos_sg, z_pos_sg, markersize=3, legend=false)
+for i in 1:N_sg
+  theta = rand(rng, Float64)*2*pi
+  phi = rand(rng,Float64)*pi
+  x_dir_sg[i] = sin(theta)cos(phi)
+  y_dir_sg[i] = sin(theta)sin(phi)
+  z_dir_sg[i] = cos(theta)
+end
 
+x_dir_sg = vec(x_dir_sg)
+y_dir_sg = vec(y_dir_sg)
+z_dir_sg = vec(z_dir_sg)
+
+#INITIALIZATION OF THE FM LATTICE
+x_pos_fm = [1.5,5.5,9.5,]
+y_pos_fm = [1.0,1.25,1.5,1.75,2.0,5.0,5.25,5.5,5.75,6.0,9.0,9.25,9.5,9.75,10.0]
+z_pos_fm = [4.0]
+Lx_fm = length(x_pos_fm)
+Ly_fm = length(y_pos_fm)
+Lz_fm = length(z_pos_fm)
+
+#FERROMAGNET SPIN POSITIONS
+x_pos_fm = repeat(x_pos_fm, inner=(Ly_fm,1))
+x_pos_fm = repeat(x_pos_fm, outer=(Lz_fm,1))
+y_pos_fm = repeat(y_pos_fm, outer=(Lx_fm,1))
+y_pos_fm = repeat(y_pos_fm, outer=(Lz_fm,1))
+z_pos_fm = repeat(z_pos_fm, inner=(Lx_fm*Ly_fm,1))
+
+x_pos_fm = vec(x_pos_fm)
+y_pos_fm = vec(y_pos_fm)
+z_pos_fm = vec(z_pos_fm)
+  
+#NUMBER OF FERROMAGNETIC SPINS
+N_fm = Lx_fm*Ly_fm*Lz_fm
+
+#FERROMAGNETIC SPIN VECTORS
+x_dir_fm = Float64[ 5.0 for i in 1:N_fm]
+y_dir_fm = Float64[ 0.0 for i in 1:N_fm]
+z_dir_fm = Float64[ 0.0 for i in 1:N_fm]
+
+#NUMBER OF TOTAL SPINS
+N_tot = N_sg+N_fm
 
 #PRINTING INITIAL CONFIGURATION (using makie)
-aspect=(1, 1, 0.5)
-perspectiveness=0.5
-fig = Figure(; resolution=(1200, 400))
-ax = Axis3(fig[1, 1]; aspect, perspectiveness)
-scatter!(ax, x_pos_sg, y_pos_sg, z_pos_sg; markersize=5, color= :red)
-scatter!(ax, x_pos_base, y_pos_base, z_pos_base; markersize=1, color= :black)
-display(fig)
+#aspect = (10, 10, 5)
+#perspectiveness = 0.5
+#fig = Figure(; resolution=(1200, 1200))
+#ax = Axis3(fig[1, 1]; aspect, perspectiveness)
+#quiver!(ax, x_pos_sg, y_pos_sg, z_pos_sg, x_dir_sg, y_dir_sg, z_dir_sg, color= :violet; arrowsize=0.1, )
+#quiver!(ax, x_pos_fm, y_pos_fm, z_pos_fm, x_dir_fm, y_dir_fm, z_dir_fm, color= :black, arrowsize=0.1)
+#scatter!(ax, x_pos_sg, y_pos_sg, z_pos_sg; markersize=5, color= :red)
+#scatter!(ax, x_pos_base, y_pos_base, z_pos_base; markersize=1, color= :cyan)
+#display(fig)
+#Makie.save("Initial_spins.png", fig)
+
+#RKKY INETRACTION J VALUE
+
+global a = Lattice_const                               #near neighbour distance (Considering the matterial to be CuMn, with Mn density to be 10%)
+global k_f = pi/a                         #2K_f*a the periodicity of RKKY
+global alpha = 2*k_f*a
+
+function RKKY_J(x_1, y_1, z_1, x_2, y_2, z_2, a, alpha)
+  J_0 = -1
+  r_ij = sqrt((x_1-x_2)^2 + (y_1-y_2)^2 + (z_1-z_2)^2)/a            #distance between spins in terms of near neighbour distance
+  term_1 = cos(alpha*r_ij)/r_ij^3
+  term_2 = sin(alpha*r_ij)/(alpha*(r_ij)^4)
+  J = J_0*(term_1-term_2)
+  return J
+end
+
+#DUMMY RKKY INTERACTION FUNCTION
+global omega = pi                          #pediodicity
+global b = 0.5                             #damping_coefficient
+function RKKY_dummy(x_1, y_1, z_1, x_2, y_2, z_2, omega, b)
+    J_0 = 2
+    r_ij = sqrt((x_1-x_2)^2 + (y_1-y_2)^2 + (z_1-z_2)^2)
+    term_1 = cos(omega*r_ij)
+    term_2 = exp(-b*r_ij)
+    J = J_0*term_1*term_2
+    return J
+end
+
+#RKKY INTERACTION MATRIX
+interac_j = zeros(N_sg,N_sg)
+
+for i in 1:N_sg
+    for j in 1:N_sg
+        if i==j
+            interac_j[i,j]=0
+        else
+            interac_j[i,j] = RKKY_J(x_pos_sg[i], y_pos_sg[i], z_pos_sg[i], x_pos_sg[j], y_pos_sg[j], z_pos_sg[j], a, alpha)
+        end
+    end
+end
+
+
+#plot(length_dist, interac_dist, label = "J distribution")
+j_dist = reshape(interac_j, (N_sg*N_sg,1))
+display(histogram(j_dist))
+#ylabel!("Interaction coefficient (J)")
+#xlims!(0,100)
+#ylims!(-0.5,1)
+
+function dumbell_energy(x_fm, y_fm, z_fm, x_sg, y_sg, z_sg, s_sg_x, s_sg_y, s_sg_z, s_fm_x, s_fm_y, s_fm_z)
+  E_0 = 1
+  q_sg_plus = 1
+  q_sg_minus = -1
+  q_fm_plus = 5
+  q_fm_minus = -5
+
+  r_fm_d_plus_x = (x_fm+(s_fm_x/2)) 
+  r_fm_d_plus_y = (y_fm+(s_fm_y)/2)
+  r_fm_d_plus_z = (z_fm+(s_fm_z)/2)
+  r_fm_d_minus_x = (x_fm-(s_fm_x/2))
+  r_fm_d_minus_y = (y_fm-(s_fm_y/2))
+  r_fm_d_minus_z = (z_fm-(s_fm_z/2))
+  r_sg_d_plus_x = (x_sg+(s_sg_x/2))
+  r_sg_d_plus_y = (y_sg+(s_sg_y)/2)
+  r_sg_d_plus_z = (z_sg+(s_sg_z/2))
+  r_sg_d_minus_x = (x_sg-(s_sg_x/2))
+  r_sg_d_minus_y = (y_sg-(s_sg_y/2))
+  r_sg_d_minus_z = (z_sg-(s_sg_z/2))
+
+  term_1_denom = sqrt((r_fm_d_plus_x - r_sg_d_plus_x)^2 + (r_fm_d_plus_y - r_sg_d_plus_y)^2 + (r_fm_d_plus_z - r_sg_d_plus_z)^2)
+  term_1 = q_fm_plus*q_sg_plus/term_1_denom
+  term_2_denom = sqrt((r_fm_d_plus_x - r_sg_d_minus_x)^2 + (r_fm_d_plus_y - r_sg_d_minus_y)^2 + (r_fm_d_plus_z - r_sg_d_minus_z)^2)
+  term_2 = q_fm_plus*q_sg_minus/term_2_denom
+  term_3_denom = sqrt((r_fm_d_minus_x - r_sg_d_minus_x)^2 + (r_fm_d_minus_y - r_sg_d_minus_y)^2 + (r_fm_d_minus_z - r_sg_d_minus_z)^2)
+  term_3 = q_fm_minus*q_sg_minus/term_3_denom
+  term_4_denom = sqrt((r_fm_d_minus_x - r_sg_d_plus_x)^2 + (r_fm_d_minus_y - r_sg_d_plus_y)^2 + (r_fm_d_minus_z - r_sg_d_plus_z)^2)
+  term_4 = q_fm_minus*q_sg_plus/term_4_denom
+
+  E = E_0*(term_1 + term_2 + term_3 + term_4)
+
+  return E
+end
+
+#MAGNETIC FIELD DUE TO BLOCKS
+block_energy_sg = zeros(N_sg, 1)
+
+for i in 1:N_sg                       #for loop for all the spin glass materials
+  for j in 1:N_fm                     #for loop for all the ferromagnetic dumbells
+    block_energy_sg[i] += dumbell_energy(x_pos_fm[j], y_pos_fm[j], z_pos_fm[j], x_pos_sg[i], y_pos_sg[i], z_pos_sg[i], x_dir_sg[i], y_dir_sg[i], z_dir_sg[i], x_dir_fm[j], y_dir_fm[j], z_dir_fm[j])          #ENERGY TERM COSIDERING DUMBELL MODEL
+  end
+end
+
+block_energy_sg = vec(block_energy_sg)
+
+#PRINTING HISTOGRAM OF ENERGY VALUES
+#histogram(block_energy_sg)
+
+#PRINTING THE ENERGY VALUES CORRESPONDING TO SPINGLASS POSITION
+#scatter!(x_pos_sg, y_pos_sg, z_pos_sg, markersize=block_energy_sg, legend=false)
+#scatter!(x_pos_fm, y_pos_fm, z_pos_fm)
