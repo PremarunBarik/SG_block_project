c079f88beffa37907d217db650959b676e893659
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2023-06-01 17:23:00 -0500

    Update buffer.jl

--- buffer.jl
+++ buffer.jl
@@ -1,37 +1,18 @@
-#REFERENCE POSITION OF THE SPIN ELEMENTS
-x_pos_sg = zeros(N_sg, 1)
-y_pos_sg = zeros(N_sg, 1)
-z_pos_sg = zeros(N_sg, 1)
+using CUDA, LinearAlgebra
 
-for i in 1:N_sg
-    x_pos_sg[i] = trunc((((i-1)% (n_x*n_y)))/n_x)+1             #10th position
-    y_pos_sg[i] = (((i-1)%(n_x*n_y))%n_y)+1                     #1th position
-    z_pos_sg[i] = trunc((i-1)/(n_x*n_y)) +1                     #100th position
+mx = collect(1:100) |> CuArray
+mx = reshape(mx, (10,10))
+
+function spatial_correlation(N_sg, replica_num)
+
+    diag_zero = fill(1, (N_sg, N_sg)) |> CuArray
+    diag_zero[diagind(diag_zero)] .= 0
+    diag_zero = repeat(diag_zero, (replica_num,1))
+
+    correlation = reshape(x_dir_sg, (N_sg, replica_num))'
+    correlation = repeat(correlation, inner = (N_sg, 1))
+    correlation = correlation .* diag_zero
+
+    correlation = x_dir_sg .* correlation
+    correlation = sum(correlation, dims=2)
 end
-
-#x_pos_sg = repeat(x_pos_sg, 1, replica_num)
-#y_pos_sg = repeat(y_pos_sg, 1, replica_num)
-#z_pos_sg = repeat(z_pos_sg, 1, replica_num)
-
-#INITIALIZATION OF THE FM LATTICE
-x_pos_fm = [1.5,5.5,9.5,]
-y_pos_fm = [1.0,1.5,2.0,5.0,5.5,6.0,9.0,9.5,10.0]
-z_pos_fm = [4.0]
-Lx_fm = length(x_pos_fm)
-Ly_fm = length(y_pos_fm)
-Lz_fm = length(z_pos_fm)
-
-#FERROMAGNET SPIN POSITIONS
-x_pos_fm = repeat(x_pos_fm, inner=(Ly_fm,1))
-x_pos_fm = repeat(x_pos_fm, outer=(Lz_fm,1))
-y_pos_fm = repeat(y_pos_fm, outer=(Lx_fm,1))
-y_pos_fm = repeat(y_pos_fm, outer=(Lz_fm,1))
-z_pos_fm = repeat(z_pos_fm, inner=(Lx_fm*Ly_fm,1))
-
-#NUMBER OF FERROMAGNETIC SPINS
-N_fm = Lx_fm*Ly_fm*Lz_fm
-
-#FERROMAGNETIC SPIN VECTORS
-x_dir_fm = Float64[ 1.0 for i in 1:N_fm]
-y_dir_fm = Float64[ 0.0 for i in 1:N_fm]
-z_dir_fm = Float64[ 0.0 for i in 1:N_fm]
