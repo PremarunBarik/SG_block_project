527e699a667d7874ab58a723798a220f4f969eb4
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2023-06-02 02:53:18 -0500

    Update buffer.jl

--- buffer.jl
+++ buffer.jl
@@ -1,18 +1,37 @@
-using CUDA, LinearAlgebra
+using LinearAlgebra
 
-mx = collect(1:100) |> CuArray
-mx = reshape(mx, (10,10))
+N_sg = 10
+mx = collect(1:N_sg*N_sg) 
+mx = reshape(mx, (N_sg,N_sg))
 
-function spatial_correlation(N_sg, replica_num)
+#Initilization inside the temp loop, before MC loop
+global corltn_term1 = zeros(N_sg*replica_num, 1)                #<sigma_i*sigma_j>
+global corltn_term2 = zeros(N_sg*replica_num, 1)                #<sigma_i>
+global corltn_term3 = zeros(N_sg*replica_num, 1)                #<sigma_j>
 
-    diag_zero = fill(1, (N_sg, N_sg)) |> CuArray
+#Put inside the MC loop, after the MC function
+function spatial_correlation_terms(N_sg, replica_num)
+
+    diag_zero = fill(1, (N_sg, N_sg)) 
     diag_zero[diagind(diag_zero)] .= 0
     diag_zero = repeat(diag_zero, (replica_num,1))
 
-    correlation = reshape(x_dir_sg, (N_sg, replica_num))'
-    correlation = repeat(correlation, inner = (N_sg, 1))
-    correlation = correlation .* diag_zero
+    corltn = reshape(x_dir_sg, (N_sg, replica_num))'
+    corltn = repeat(corltn, inner = (N_sg, 1))
+    corltn = corltn .* diag_zero
 
-    correlation = x_dir_sg .* correlation
-    correlation = sum(correlation, dims=2)
+    corltn_term1 += x_dir_sg .* corltn
+   
+    term2 = repeat(x_dir_sg, (1, N_sg))
+    corltn_term2 += term2 .* diag_zero
+
+    corltn_term3 += corltn
+end
+
+#put outside the MC loop, just at the end of MC loop, Inside the temp loop
+function spatial_correlation_claculation(MC_steps, N_sg, replica_num)
+    sp_corltn = (corltn_term1/MC_steps) .- ((corltn_term2/MC_steps) .* (corltn_term3/MC_steps))
+    sp_corltn = sum(sp_corltn)/((N_sg -1)*N_sg*replica_num)
+
+    return sp_corltn
 end
