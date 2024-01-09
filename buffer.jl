80dfa15c642883da6d254ad4d126f5d0c399d95e
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2024-01-09 13:52:33 -0600

    Update buffer.jl

--- buffer.jl
+++ buffer.jl
@@ -1,130 +1,74 @@
 using CUDA, Random, Plots, LinearAlgebra, BenchmarkTools
 
-#To change the magnetic field intensity: goto 'compute_dummbell_energy' function
-#To change the density and orientation of ferromagnetic blocks go to 'Initialization of ferromagnetic blocks'
-#This script applies only with 3 or more than 3 layers of spin glass material
-#Although debatable - 3D EA model transition temperature is between 0.9 - 1.2
-
-#FERROMAGNETIC BLOCK FIELD INTENSITY
-global field_intensity = 0.0
-
 rng = MersenneTwister()
 
-#NUMBER OF REPLICAS 
-replica_num = 50
+#considered system size
+n_x = 3
+n_y = 3
 
-#NUMBER OF MC MC STEPS 
-MC_steps = 100000
-MC_burns = 100000
+#N_lattice = (Nx * Ny)
 
-#TEMPERATURE VALUES
-min_Temp = 0.1
-max_Temp = 2.0
-Temp_step = 50
-Temp_interval = (max_Temp - min_Temp)/Temp_step
-Temp_values = collect(min_Temp:Temp_interval:max_Temp)
-Temp_values = reverse(Temp_values)
+#consideres percentage of msgnetic spin
+percent = 10
 
-#------------------------------------------------------------------------------------------------------------------------------#
+#number of spins
+#N_sg = (N_lattice * percent)/100  |> Int64
+N_sg = n_x * n_y
 
-#NUMBER OF SPINGLASS ELEMENTS
-n_x = 10
-n_y = 10
-n_z = 1
-
-N_sg = n_x*n_y
-
-#SPIN ELEMENT DIRECTION IN REPLICAS
+#spin element directions
 x_dir_sg = [(-1)^rand(rng, Int64) for i in 1:N_sg]
 
-#y_dir_sg = zeros(N_sg, 1)
-#z_dir_sg = zeros(N_sg, 1)
-
-#spin initialization -- for heisenberg spins -- need to chnage in the CuArray section -- need to change in the dummbell energy function
-#for i in 1:N_sg
-#    theta = pi/2
-#    phi = 0
-#    x_dir_sg[i] = sin(theta)cos(phi)
-#    y_dir_sg[i] = sin(theta)sin(phi)
-#    z_dir_sg[i] = cos(theta)
-#end
-#spin initialization -- for ising spins
-
-x_dir_sg = repeat(x_dir_sg, replica_num, 1)
-#y_dir_sg = repeat(y_dir_sg, replica_num, 1)
-#z_dir_sg = repeat(z_dir_sg, replica_num, 1)
-
-#------------------------------------------------------------------------------------------------------------------------------#
+#selecting spins in random positions
+random_position = randperm(Nx*Ny)
 
 #REFERENCE POSITION OF THE SPIN ELEMENTS IN MATRIX
-mx_sg = CuArray(collect(1:N_sg*replica_num))
+mx_sg = collect(1:N_sg)
+mx_sg = shuffle!(mx_sg)
 
 #REFERENCE POSITION OF THE SPIN ELEMENTS IN GEOMETRY
 x_pos_sg = zeros(N_sg, 1)
 y_pos_sg = zeros(N_sg, 1)
-z_pos_sg = fill(n_z, N_sg)
+z_pos_sg = fill(1, N_sg)
 
 for i in 1:N_sg
-    x_pos_sg[i] = trunc((i-1)/n_x)+1                    #10th position
-    y_pos_sg[i] = ((i-1)%n_y)+1                         #1th position
+    x_pos_sg[i] = trunc((mx_sg[i]-1)/n_x)+1                    #10th position
+    y_pos_sg[i] = ((mx_sg[i]-1)%n_y)+1                         #1th position
 end
 
-#------------------------------------------------------------------------------------------------------------------------------#
+display(scatter(x_pos_sg, y_pos_sg))
 
-#INITIALIZATION OF FERROMAGNETIC BLOCKS
-x_num = 2                                                       #number of blocks along X axis 
-y_num = 2                                                       #number of blocks along Y axis
-N_fm = x_num*y_num
+#matrix to keep track of interaction coefficients.
+J_NN = zeros(N_sg, N_sg, replica_num)
 
-x_dist = n_x/x_num                                              #distance between two blocks along x axis 
-y_dist = n_y/y_num                                              #distance between two blocks along y axis 
-
-#REFERENCE POSITION OF THE FERROMAGNETIC BLOCKS IN MATRIX
-mx_fm = collect(1:N_fm) |> CuArray
-
-#LENGTH OF FERROMAGNETIC DIPOLE 
-fm_length = 3
-
-#REFERENCE POSITION OF THE BLOCKS
-x_pos_fm = zeros(N_fm, 1)
-y_pos_fm = zeros(N_fm, 1)
-z_pos_fm = fill(n_z + 1, N_fm) 
-
-for i in 1:N_fm
-    x_pos_fm[i] = trunc((i-1)/x_num)*(x_dist) + (x_dist/2)                  #10th position
-    y_pos_fm[i] = ((i-1)%y_num)*(y_dist) + (y_dist/2)                       #1th position
-end
-
-global positive_x_pos_fm = x_pos_fm .+ (fm_length/2)
-global negative_x_pos_fm = x_pos_fm .- (fm_length/2)
-
-global positive_y_pos_fm = y_pos_fm
-global negative_y_pos_fm = y_pos_fm
-
-global positive_z_pos_fm = z_pos_fm
-global negative_z_pos_fm = z_pos_fm
+#matrix to keep track of interaction coefficients
+J_NN_counter = zeros(N_sg, N_sg, replica_num)
 
 #------------------------------------------------------------------------------------------------------------------------------#
 
 #ISING NEAR NEIGHBOUR CALCULATION
-NN_s = zeros(N_sg,1)
-NN_n = zeros(N_sg,1)
-NN_e = zeros(N_sg,1)
-NN_w = zeros(N_sg,1)
+NN_s = zeros(N_sg,replica_num)
+NN_n = zeros(N_sg,replica_num)
+NN_e = zeros(N_sg,replica_num)
+NN_w = zeros(N_sg,replica_num)
 
-#ISING NEAR NEIGHBOUR CALCULATION
-NN_s = zeros(N_sg,1)
-NN_n = zeros(N_sg,1)
-NN_e = zeros(N_sg,1)
-NN_w = zeros(N_sg,1)
-
+for k in 1: replica_num
 for i in 1:N_sg                             #loop over all the spin ELEMENTS
-        if x_pos_sg[i]%n_x == 0
-            r_e =  (x_pos_sg[i]-n_x)*n_x + y_pos_sg[i]
+        if x_pos_sg[i,k]%n_x == 0
+            r_e =  (x_pos_sg[i,k]-n_x)*n_x + y_pos_sg[i,k]
         else
-            r_e =  x_pos_sg[i]*n_x + y_pos_sg[i]
+            r_e =  x_pos_sg[i,k]*n_x + y_pos_sg[i,k]
         end
-        NN_e[i] = r_e
+
+        for j in 1:N_sg
+            index_1 = (j-1)*N_sg + i
+            index_2 = (i-1)*N_sg + j
+            if mx_sg[j]==r_e && J_NN_counter[index_1]==0 && J_NN_counter[index_2]==0
+                J_NN[index_1] = J_NN[index_2] = rand(rng, Int64)
+                J_NN_counter[index_1] += 1
+                J_NN_counter[index_2] += 1
+                NN_e[i,k] = j
+            end
+        end
         #-----------------------------------------------------------#
         if x_pos_sg[i]%n_x == 1
             r_w = (x_pos_sg[i]+n_x-2)*n_x + y_pos_sg[i]
@@ -132,6 +76,15 @@ for i in 1:N_sg                             #loop over all the spin ELEMENTS
             r_w = (x_pos_sg[i]-2)*n_x + y_pos_sg[i]
         end
         NN_w[i] = r_w
+        for j in 1:N_sg
+            index_1 = (j-1)*N_sg + i
+            index_2 = (i-1)*N_sg + j
+            if mx_sg[j]==r_w && J_NN_counter[index_1]==0 && J_NN_counter[index_2]==0
+                J_NN[index_1] = J_NN[index_2] = rand(rng, Int64)
+                J_NN_counter[index_1] += 1
+                J_NN_counter[index_2] += 1
+            end
+        end
         #-----------------------------------------------------------#
         if y_pos_sg[i]%n_y == 0
             r_n =  (x_pos_sg[i]-1)*n_x + (y_pos_sg[i]-n_y+1)
@@ -139,6 +92,15 @@ for i in 1:N_sg                             #loop over all the spin ELEMENTS
             r_n = (x_pos_sg[i]-1)*n_x + y_pos_sg[i]+1
         end
         NN_n[i] = r_n
+        for j in 1:N_sg
+            index_1 = (j-1)*N_sg + i
+            index_2 = (i-1)*N_sg + j
+            if mx_sg[j]==r_n && J_NN_counter[index_1]==0 && J_NN_counter[index_2]==0
+                J_NN[index_1] = J_NN[index_2] = rand(rng, Int64)
+                J_NN_counter[index_1] += 1
+                J_NN_counter[index_2] += 1
+            end
+        end
         #-----------------------------------------------------------#
         if y_pos_sg[i]%n_y == 1
             r_s = (x_pos_sg[i]-1)*n_x + (y_pos_sg[i]+n_y-1)
@@ -146,223 +108,14 @@ for i in 1:N_sg                             #loop over all the spin ELEMENTS
             r_s = (x_pos_sg[i]-1)*n_x + y_pos_sg[i]-1
         end
         NN_s[i] = r_s
-end
-
-NN_s = repeat(NN_s, replica_num, 1)
-NN_n = repeat(NN_n, replica_num, 1)
-NN_e = repeat(NN_e, replica_num, 1)
-NN_w = repeat(NN_w, replica_num, 1)
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#INTERACTION COEFFICIENT MATRIX
-J_NN = zeros(N_sg,N_sg,replica_num)
-
-for i in 1:N_sg
-    for j in i:N_sg
-        for k in 1:replica_num
-            if i==j
-                continue
-            else
-                J_NN[i,j,k] = J_NN[j,i,k] = (-1)^rand(rng, Int64)                                   #for ising: 1, for spin glas: random
+        for j in 1:N_sg
+            index_1 = (j-1)*N_sg + i
+            index_2 = (i-1)*N_sg + j
+            if mx_sg[j]==r_s && J_NN_counter[index_1]==0 && J_NN_counter[index_2]==0
+                J_NN[index_1] = J_NN[index_2] = rand(rng, Int64)
+                J_NN_counter[index_1] += 1
+                J_NN_counter[index_2] += 1
             end
         end
-    end
 end
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#REPLICA REFERENCE MATRIX OF SPIN ELEMENTS
-spin_rep_ref = zeros(N_sg*replica_num,1)
-
-for i in eachindex(spin_rep_ref)
-    spin_rep_ref[i] = trunc((i-1)/N_sg)*N_sg
-end
-
-#REPLICA REFERENCE MATRIX OF RANDOMLY SELECTED SPIN IN MC_STEP
-rand_rep_ref = zeros(replica_num, 1)
-
-for i in eachindex(rand_rep_ref)
-    rand_rep_ref[i] = (i-1)*N_sg
-end
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#CHANGING ALL THE MATRICES TO CU_ARRAY 
-global x_dir_sg = CuArray(x_dir_sg)
-#global y_dir_sg = CuArray(y_dir_sg)
-#global z_dir_sg = CuArray(z_dir_sg)
-
-global x_pos_sg = Array(x_pos_sg .- 0.5)                                  #fixing the exact position of 
-global y_pos_sg = Array(y_pos_sg .- 0.5)
-global z_pos_sg = Array(z_pos_sg)
-
-global x_pos_fm = Array(x_pos_fm)
-global y_pos_fm = Array(y_pos_fm)
-global z_pos_fm = Array(z_pos_fm)
-
-NN_s = CuArray{Int64}(NN_s)
-NN_n = CuArray{Int64}(NN_n)
-NN_e = CuArray{Int64}(NN_e)
-NN_w = CuArray{Int64}(NN_w)
-
-J_NN = CuArray(J_NN)
-
-spin_rep_ref = CuArray{Int64}(spin_rep_ref)
-rand_rep_ref = CuArray{Int64}(rand_rep_ref)
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#MATRIX TO STORE ENERGY DUE TO MAGNETIC BLOCKS
-global dipole_field = zeros(N_sg*replica_num, 1) |> CuArray
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#CALCULATION OF ENERGY DUE TO FERROMAGNETIC BLOCKS AS DIPOLES
-function calculate_dipole_field()
-    positive_distance = sqrt.( ((x_pos_sg .- positive_x_pos_fm').^2) .+ ((y_pos_sg .- positive_y_pos_fm').^2) .+ ((z_pos_sg .- positive_z_pos_fm').^2))
-    negative_distance = sqrt.( ((x_pos_sg .- negative_x_pos_fm').^2) .+ ((y_pos_sg .- negative_y_pos_fm').^2) .+ ((z_pos_sg .- negative_z_pos_fm').^2))
-
-    q_positive = field_intensity
-    q_negative = -field_intensity
-
-    B_x_positive = q_positive *(x_pos_sg .- positive_x_pos_fm')./(positive_distance.^3)
-    B_x_negative = q_negative *(x_pos_sg .- negative_x_pos_fm')./(negative_distance.^3)
-
-    B_x_tot = B_x_positive .+ B_x_negative
-
-    dipole_field = sum(B_x_tot, dims=2) 
-    global dipole_field = repeat(dipole_field, replica_num, 1) |> CuArray
-
-    return dipole_field
-end
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#MATRIX TO STORE ENERGY DUE TO RKKY 
-global energy_RKKY = zeros(N_sg*replica_num, 1) |> CuArray
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#COMPUTE TOTAL ENERGY OF THE SYSTEM
-function compute_RKKY_energy_spin_glass()
-    r_s = (mx_sg.-1).*N_sg .+ NN_s
-    r_n = (mx_sg.-1).*N_sg .+ NN_n 
-    r_e = (mx_sg.-1).*N_sg .+ NN_e 
-    r_w = (mx_sg.-1).*N_sg .+ NN_w 
-
-    energy_x = x_dir_sg.*((J_NN[r_s].*x_dir_sg[NN_s .+ spin_rep_ref]) .+ (J_NN[r_n].*x_dir_sg[NN_n .+ spin_rep_ref]) .+ (J_NN[r_e].*x_dir_sg[NN_e .+ spin_rep_ref]) .+ (J_NN[r_w].*x_dir_sg[NN_w .+ spin_rep_ref]))
-   
-    global energy_RKKY = energy_x
-
-    return energy_RKKY
-end
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#MATRIX TO STORE TOTAL ENERGY
-global energy_tot = zeros(N_sg*replica_num, 1) |> CuArray
-#MATRIX TO STORE DELTA ENERGY
-global del_energy = zeros(replica_num, 1) |> CuArray
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#COMPUTE THE ENERGY CHANGE OF THE SYSTEM
-function compute_del_energy_spin_glass(rng)
-    compute_RKKY_energy_spin_glass()
-
-    global energy_tot = 2*energy_RKKY .+ (dipole_field .* x_dir_sg)                         #CALCULATING TOTAL ENERGY OF SPIN GLASS LOCAL MOMENTS
-
-    global rand_pos =  CuArray(rand(rng, (1:N_sg), (replica_num, 1)))
-    global r = rand_pos .+ rand_rep_ref
-
-    global del_energy = energy_tot[r]
-
-    return del_energy
-end
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#MATRIX TO STORE DELTA ENERGY
-global trans_rate = CuArray(zeros(replica_num, 1))
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#ONE MC STEPS
-function one_MC(rng, Temp)                                           #benchmark time: 1.89ms (smallest)
-    compute_del_energy_spin_glass(rng)
-
-    global trans_rate = exp.(-del_energy/Temp)
-    global rand_num_flip = CuArray(rand(rng, Float64, (replica_num, 1)))
-    flipit = sign.(rand_num_flip .- trans_rate)
-
-    global x_dir_sg[r] = flipit.*x_dir_sg[r]
-end
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#creating a matrix with zero diagonal terms  to calculate the correlation terms
-diag_zero = fill(1, (N_sg, N_sg)) |> Array
-diag_zero[diagind(diag_zero)] .= 0
-global  diag_zero = repeat(diag_zero, replica_num, 1) |> CuArray
-
-#Put inside the MC loop, after the MC function - Calculation of spatial correlation function terms
-function spatial_correlation_terms(N_sg, replica_num)
-    spin_mux = reshape(x_dir_sg, (N_sg, replica_num))'  |>  Array
-    spin_mux = repeat(spin_mux, inner = (N_sg, 1))                                  #Scalar indexing - less time consum$
-    spin_mux = spin_mux |> CuArray
-
-    global term_1 += x_dir_sg .* spin_mux .* diag_zero                                           #sum of sigma_i*sigma_j
-end
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#MATRIX FOR STORING DATA
-susceptibility = zeros(length(Temp_values), 1)
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#MAIN BODY
-calculate_dipole_field()                                                       #CALCULATION OF MAGNETIC FIELD LINES ONE TIME AND IT WILL NOT CHANGE OVER TIME
-
-@CUDA.allowscalar for i in eachindex(Temp_values)                               #TEMPERATURE LOOP 
-    
-    global Temp_index = i
-    global Temp = Temp_values[Temp_index] 
-
-    #MC BURN STEPS
-    @CUDA.allowscalar for j in 1:MC_burns
-
-        one_MC(rng, Temp)
-
-    end
-
-    #-----------------------------------------------------------#
-
-    #Initilization inside the temp loop, before MC loop - Calculation of spatial correlation function
-    global term_1 = zeros(N_sg*replica_num, N_sg) |> CuArray                  #<sigma_i*sigma_j>
-
-    #-----------------------------------------------------------#
-
-    #MONTE CARLO FUNCTION
-    @CUDA.allowscalar for j in 1:MC_steps
-
-        one_MC(rng, Temp)
-        spatial_correlation_terms(N_sg, replica_num)                                                    
-
-    end
-    #-----------------------------------------------------------#
-
-    susceptibility_replica = sum(term_1, dims=2)/MC_steps
-    susceptibility_replica = Array(sum(reshape(susceptibility_replica, (N_sg,replica_num)), dims=1)) .^ 2
-    susceptibility[Temp_index] = sum(susceptibility_replica)/replica_num
-end
-
-#------------------------------------------------------------------------------------------------------------------------------#
-
-#SAVING THE GENERATED DATA
-open("2D_EA_suscep_10x10_MC100K_B0.0.txt", "w") do io 					#creating a file to save data
-   for i in 1:length(Temp_values)
-      println(io,i,"\t", Temp_values[i],"\t", susceptibility[i],"\t", magnetization[i], "\t", EA_order_parameter[i])
-   end
 end
