86dfda715e63b921639ea47ec53f88ab2a4379bc
PremarunBarik <106409223+PremarunBarik@users.noreply.github.com>
2024-01-23 13:16:28 -0600

    Update buffer_test.jl

--- buffer_test.jl
+++ buffer_test.jl
@@ -1,61 +1,45 @@
-using Random, Plots, LinearAlgebra, BenchmarkTools
+using CUDA, Random, Plots, LinearAlgebra, BenchmarkTools
 
 rng = MersenneTwister()
 
 #considered lattice size 
-n_x = 10
-n_y = 10
+N_x = 30
+N_y = 30
 
-N_lattice = (n_x * n_y)
-
-#number of replicas
-replica_num = 10
+N_lattice = (N_x * N_y)
 
 #consideres percentage of magnetic spin
-percent = 20
+percent = 10
 
 #number of spins
 N_sg = (N_lattice * percent)/100  |> Int64
 #N_sg = n_x * n_y
 
-#initializing the spin element direction in replica lattices
-
-#spin directions in spin-direction list in one replica
+#spin element directions in magnetic elements list
 #x_dir_sg = [(-1)^rand(rng, Int64) for i in 1:N_sg]
 x_dir_sg = collect(1:N_sg)
-#repeating the spin direction among replicas
-x_dir_sg = repeat(x_dir_sg, 1, replica_num)
 
+#selecting spins in random positions
+random_position = randperm(N_lattice)
 
-#selecting random spin positions in replica lattices
-mx_sg = zeros(N_sg, replica_num)
-for k in 1:replica_num
-    #shuffling the lattice positions to generate a random list
-    random_position = randperm(N_lattice)
-
-    #selecting spins in random positions in one single replica
-    mx_sg[:,k] = random_position[1:N_sg]
-end
+#REFERENCE POSITION OF THE SPIN ELEMENTS IN MATRIX
+mx_sg = random_position[1:N_sg]
 
 #REFERENCE POSITION OF THE SPIN ELEMENTS IN GEOMETRY
-x_pos_sg = zeros(N_sg, replica_num)
-y_pos_sg = zeros(N_sg, replica_num)
-z_pos_sg = ones(N_sg, replica_num)
+x_pos_sg = zeros(N_sg, 1)
+y_pos_sg = zeros(N_sg, 1)
+z_pos_sg = fill(1, N_sg)
 
-for k in 1: replica_num
-    for i in 1:N_sg
-        x_pos_sg[i,k] = trunc((mx_sg[i,k]-1)/n_x)+1                    #10th position
-        y_pos_sg[i,k] = ((mx_sg[i,k]-1)%n_y)+1                         #1th position
-    end
+for i in 1:N_sg
+    x_pos_sg[i] = trunc((mx_sg[i]-1)/n_x)+1                    #10th position
+    y_pos_sg[i] = ((mx_sg[i]-1)%n_y)+1                         #1th position
 end
 
 #display(scatter(x_pos_sg, y_pos_sg))
 
 #reference of spin directions in lattice
-x_dir_lattice = zeros(N_lattice, replica_num)
-lattice_rep_ref = collect(0:N_lattice:N_lattice*replica_num)
-lattice_rep_ref = repeat(lattice_rep_ref', N_sg,1)
-x_dir_lattice[mx_sg .+ lattice_rep_ref] += x_dir_sg
+x_dir_lattice = zeros(N_lattice,1)
+x_dir_lattice[mx_sg] += x_dir_sg
 
 #matrix to keep track of interaction coefficients.
 J_NN = zeros(N_sg, N_sg)
@@ -66,35 +50,32 @@ J_NN_counter = zeros(N_sg, N_sg)
 #------------------------------------------------------------------------------------------------------------------------------#
 
 #ISING NEAR NEIGHBOUR CALCULATION
-NN_s = zeros(N_sg, 1)
-NN_n = zeros(N_sg, 1)
-NN_e = zeros(N_sg, 1)
-NN_w = zeros(N_sg, 1)
+NN_s = zeros(N_sg,replica_num)
+NN_n = zeros(N_sg,replica_num)
+NN_e = zeros(N_sg,replica_num)
+NN_w = zeros(N_sg,replica_num)
 
-J_NN_s = zeros(N_sg, 1)
-J_NN_n = zeros(N_sg, 1)
-J_NN_e = zeros(N_sg, 1)
-J_NN_w = zeros(N_sg, 1)
+J_NN_s = zeros(N_sg,1)
+J_NN_n = zeros(N_sg,1)
+J_NN_e = zeros(N_sg,1)
+J_NN_w = zeros(N_sg,1)
 
-J_NN_s_counter = zeros(N_sg, 1)
-J_NN_n_counter = zeros(N_sg, 1)
-J_NN_e_counter = zeros(N_sg, 1)
-J_NN_w_counter = zeros(N_sg, 1)
+J_NN_s_counter = zeros(N_sg,1)
+J_NN_n_counter = zeros(N_sg,1)
+J_NN_e_counter = zeros(N_sg,1)
+J_NN_w_counter = zeros(N_sg,1)
 
-Interaction_mx = Array{Float64}(undef,0)
-
+for k in 1: replica_num
 for i in 1:N_sg                             #loop over all the spin ELEMENTS
-        if x_pos_sg[i]%n_x == 0
-            r_e =  (x_pos_sg[i]-n_x)*n_x + y_pos_sg[i]
+        if x_pos_sg[i,k]%n_x == 0
+            r_e =  (x_pos_sg[i,k]-n_x)*n_x + y_pos_sg[i,k]
         else
-            r_e =  x_pos_sg[i]*n_x + y_pos_sg[i]
+            r_e =  x_pos_sg[i,k]*n_x + y_pos_sg[i,k]
         end
-        NN_e[i] = r_e
+        NN_e[i,k] = r_e
         for j in 1:N_sg
             if mx_sg[j]==r_e && J_NN_e_counter[i]==0 && J_NN_w_counter[j]==0
-                J_NN_e[i] = J_NN_w[j] = randn()
-                J_NN_e_counter[i] = J_NN_w_counter[j] = 1
-                push!(Interaction_mx, J_NN_e[i])
+                J_NN_e[i] = J_NN_w[j] = rand(rng, Int64)
             end
         end
         #-----------------------------------------------------------#
@@ -105,10 +86,12 @@ for i in 1:N_sg                             #loop over all the spin ELEMENTS
         end
         NN_w[i] = r_w
         for j in 1:N_sg
-            if mx_sg[j]==r_w && J_NN_w_counter[i]==0 && J_NN_e_counter[j]==0
-                J_NN_w[i] = J_NN_e[j] = randn()
-                J_NN_w_counter[i] = J_NN_e_counter[j] = 1
-                push!(Interaction_mx, J_NN_w[i])
+            index_1 = (j-1)*N_sg + i
+            index_2 = (i-1)*N_sg + j
+            if mx_sg[j]==r_w && J_NN_counter[index_1]==0 && J_NN_counter[index_2]==0
+                J_NN[index_1] = J_NN[index_2] = rand(rng, Int64)
+                J_NN_counter[index_1] += 1
+                J_NN_counter[index_2] += 1
             end
         end
         #-----------------------------------------------------------#
@@ -119,10 +102,12 @@ for i in 1:N_sg                             #loop over all the spin ELEMENTS
         end
         NN_n[i] = r_n
         for j in 1:N_sg
-            if mx_sg[j]==r_n && J_NN_n_counter[i]==0 && J_NN_s_counter[j]==0
-                J_NN_n[i] = J_NN_s[j] = randn()
-                J_NN_n_counter[i] = J_NN_s_counter[j] = 1
-                push!(Interaction_mx, J_NN_n[i])
+            index_1 = (j-1)*N_sg + i
+            index_2 = (i-1)*N_sg + j
+            if mx_sg[j]==r_n && J_NN_counter[index_1]==0 && J_NN_counter[index_2]==0
+                J_NN[index_1] = J_NN[index_2] = rand(rng, Int64)
+                J_NN_counter[index_1] += 1
+                J_NN_counter[index_2] += 1
             end
         end
         #-----------------------------------------------------------#
@@ -133,18 +118,13 @@ for i in 1:N_sg                             #loop over all the spin ELEMENTS
         end
         NN_s[i] = r_s
         for j in 1:N_sg
-            if mx_sg[j]==r_s && J_NN_s_counter[i]==0 && J_NN_n_counter[j]==0
-                J_NN_s[i] = J_NN_n[j] = randn()
-                J_NN_s_counter[i] = J_NN_n_counter[j] = 1
-                push!(Interaction_mx, J_NN_s[i])
+            index_1 = (j-1)*N_sg + i
+            index_2 = (i-1)*N_sg + j
+            if mx_sg[j]==r_s && J_NN_counter[index_1]==0 && J_NN_counter[index_2]==0
+                J_NN[index_1] = J_NN[index_2] = rand(rng, Int64)
+                J_NN_counter[index_1] += 1
+                J_NN_counter[index_2] += 1
             end
         end
 end
-
-Interaction_mx_sort = sort!(abs.(Interaction_mx))
-Interaction_mx = Interaction_mx./Interaction_mx_sort[length(Interaction_mx)]
-
-J_NN_s = J_NN_s./Interaction_mx_sort[length(Interaction_mx)]
-J_NN_n = J_NN_n./Interaction_mx_sort[length(Interaction_mx)]
-J_NN_e = J_NN_e./Interaction_mx_sort[length(Interaction_mx)]
-J_NN_w = J_NN_w./Interaction_mx_sort[length(Interaction_mx)]
+end
